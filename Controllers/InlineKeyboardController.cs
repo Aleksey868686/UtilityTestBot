5ba5e18cf69a2120e948199d37df332a456930ce
avvakumov <avvakumov@tslog.ru>
2023-01-13 18:47:20 +0300

    Итоговый коммит. Разработка бота завершена.

--- Controllers/InlineKeyboardController.cs
+++ Controllers/InlineKeyboardController.cs
@@ -1,22 +1,53 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using UtilityTestBot.Services;
 
 namespace UtilityTestBot.Controllers
 {
     internal class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
+        private readonly TextMessageController _textMessageController;
 
-        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage, TextMessageController textMessageController)
         {
             _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+            _textMessageController = textMessageController;
         }
 
         public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            if (callbackQuery?.Data == null)
+                return;
 
-            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+            // Обновление пользовательской сессии новыми данными
+            _memoryStorage.GetSession(callbackQuery.From.Id).OptionCode = callbackQuery.Data;
+
+            // Генерим информационное сообщение
+            string options = callbackQuery.Data switch
+            {
+                "Подсчет" => " Подсчет введенных символов",
+                "Сложение" => " Сложение чисел, введенных через пробел",
+                _ => String.Empty
+            };
+
+            // Отправляем в ответ уведомление о выборе
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Выбрана опция - {options}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
+            if (options == " Подсчет введенных символов")
+            {
+                await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $" Введите символы для подсчета", cancellationToken: ct);
+            }
+            else if (options == " Сложение чисел, введенных через пробел")
+            {
+                await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $" Введите числа через пробел для подсчета суммы", cancellationToken: ct);
+            }
         }
     }
 }
--- Controllers/TextMessageController.cs
+++ Controllers/TextMessageController.cs
@@ -2,44 +2,60 @@
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using UtilityTestBot.Services;
 
 namespace UtilityTestBot.Controllers
 {
     internal class TextMessageController
     {
         private readonly ITelegramBotClient _telegramClient;
+        private readonly IStorage _memoryStorage;
 
-        public TextMessageController(ITelegramBotClient telegramBotClient)
+        public TextMessageController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
         {
             _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
         }
 
         public async Task Handle(Message message, CancellationToken ct)
         {
-            switch (message.Text)
+            if (_memoryStorage.GetSession(message.Chat.Id).OptionCode == "Подсчет" && message.Text != "/start")
             {
-                case "/start":
-
-                    // Объект, представляющий кноки
-                    var buttons = new List<InlineKeyboardButton[]>();
-                    buttons.Add(new[]
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"В Вашем сообщении {message?.Text?.Length} символов)", cancellationToken: ct);
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $" Введите символы для подсчета", cancellationToken: ct);
+            }
+            else if (_memoryStorage.GetSession(message.Chat.Id).OptionCode == "Сложение" && message.Text != "/start")
+            {
+                string[] splittedNumbers = message.Text.Split(' ');
+                int sum = 0;
+                foreach (string digit in splittedNumbers)
+                {
+                    if (int.TryParse(digit, out int number) == true)
                     {
+                        sum += number;
+                    }
+                }
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Сумма чисел: {sum}", cancellationToken: ct);
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Введите цифры через пробел для подсчета суммы", cancellationToken: ct);
+            }
+            else if (message.Text == "/start")
+            {
+                // Объект, представляющий кноки
+                var buttons = new List<InlineKeyboardButton[]>();
+                buttons.Add(new[]
+                {
                         InlineKeyboardButton.WithCallbackData($" Подсчет символов" , $"Подсчет"),
                         InlineKeyboardButton.WithCallbackData($" Сложение чисел" , $"Сложение")
                     });
 
-                    // передаем кнопки вместе с сообщением (параметр ReplyMarkup)
-                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот умеет считать введенные символы или складывать числа, введенные через пробел.</b> {Environment.NewLine}",
-                        cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
-
-                    break;
-                default:
-                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Введите текст или числа через пробел.", cancellationToken: ct);
-                    break;
+                // передаем кнопки вместе с сообщением (параметр ReplyMarkup)
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот умеет считать введенные символы или складывать числа, введенные через пробел.</b> {Environment.NewLine}",
+                    cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
             }
-
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
-            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+            else
+            {
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Введите текст или числа через пробел.", cancellationToken: ct);
+            }
         }
     }
 }
